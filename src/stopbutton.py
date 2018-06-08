fa6ca6e5adc99414ce4ad145c64b5b73b6822ca1
Caggegi <rosariocaggegi99@gmail.com>
2018-06-08 20:39:28 +0200

    Update stopbutton.py

--- src/stopbutton.py
+++ src/stopbutton.py
@@ -1,3 +1,8 @@
+#
+#--------------Caggegi's Version--------------
+#-----for my Elegoo touch screen 3.5 inch-----
+#-----------Change button to GPIO20-----------
+
 #!/usr/bin/env python
 import time
 import os
@@ -8,10 +13,10 @@ from actions import stop
 
 GPIO.setmode(GPIO.BCM)
 GPIO.setwarnings(False)
-GPIO.setup(23, GPIO.IN, pull_up_down = GPIO.PUD_UP)
+GPIO.setup(20, GPIO.IN, pull_up_down = GPIO.PUD_UP)
 
-while GPIO.input(23):
+while GPIO.input(20):
     time.sleep(0.01)
-    if not GPIO.input(23):
+    if not GPIO.input(20):
        print('Stopped')
        stop() 
